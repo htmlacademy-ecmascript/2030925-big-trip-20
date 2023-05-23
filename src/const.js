b9d578c8d6692f727735b10b7f4641a9f7e392f9
GeorgijSosedov <MarmeladOFForiginal@mail.ru>
2023-05-23 21:40:31 +0500

    test homework

---
+++ src/const.js
@@ -0,0 +1,22 @@
+import { getRandomNumber } from './utils';
+
+export const EVENTS_TYPE = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+
+export const EVENTS_COUNT = 5;
+
+export const DESCTIPTIONS = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  'Cras aliquet varius magna, non porta ligula feugiat eget.',
+  'Fusce tristique felis at fermentum pharetra.',
+  'Aliquam id orci ut lectus varius viverra.',
+  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.'
+];
+
+export const TITLES = ['Add luggage','Switch to comfort','Lunch in city','Book tickets','Add breakfast', 'Add meal', 'Order Uber', 'Choose seats'];
+
+export const CITIES = ['Geneva', 'Chamonix', 'Paris', 'Brussels', 'Prague', 'Ufa', 'Los-Santos', 'San-Fierro', 'Vice-city', 'Las-Venturas', 'London'];
+
+export const PHOTOS = `https://loremflickr.com/248/152?random=${getRandomNumber(1,99)}`;
+
+export const PRICES = getRandomNumber(10,50);
+
